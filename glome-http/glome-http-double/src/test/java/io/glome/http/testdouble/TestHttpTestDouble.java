cf13625de1b8aea8302496ad7a8b98f5d796af10
michaellif <michael.lifschitz@gmail.com>
2017-10-09 21:44:45 -0400

    impl

---
+++ glome-http/glome-http-double/src/test/java/io/glome/http/testdouble/TestHttpTestDouble.java
@@ -0,0 +1,39 @@
+package io.glome.http.testdouble;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.InetAddress;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ratpack.http.HttpUrlBuilder;
+import ratpack.http.client.HttpClient;
+import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfig;
+import ratpack.test.embed.EmbeddedApp;
+
+public class TestHttpTestDouble {
+
+	@BeforeClass
+	public static void runTestServer() throws Exception {
+		RatpackServer.start(serverSpec -> serverSpec //
+				.serverConfig(ServerConfig.builder().address(InetAddress.getLoopbackAddress()).port(8888).build())
+				.handlers( //
+						chain -> chain.get(ctx -> ctx.render("Hello Glome")) //
+								.get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!")) //
+		));
+
+	}
+
+	@Test
+	public void testSimpleClient() throws Exception {
+		EmbeddedApp.fromHandler(ctx -> ctx.render(ctx.get(HttpClient.class).get( //
+				HttpUrlBuilder.http().host(InetAddress.getLoopbackAddress().getHostAddress()).port(8888).build())
+				.map(response -> response.getBody().getText())))
+				.test(httpClient -> assertEquals("Hello Glome", httpClient.getText()));
+
+	}
+	
+	
+}
