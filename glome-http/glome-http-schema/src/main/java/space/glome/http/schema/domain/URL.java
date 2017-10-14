20d129545462f9dbb39bf104d56111f65cd2593b
michaellif <michael.lifschitz@gmail.com>
2017-10-14 19:19:40 -0400

    Implementing json unmarshalling

--- glome-http/glome-http-schema/src/main/java/space/glome/http/schema/domain/URL.java
+++ glome-http/glome-http-schema/src/main/java/space/glome/http/schema/domain/URL.java
@@ -7,9 +7,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
 @JsonSubTypes({
-    @JsonSubTypes.Type(value = RawURL.class, name = "RawURL"),
+    @JsonSubTypes.Type(value = RawURL.class, name = "raw-url"),
 
-    @JsonSubTypes.Type(value = CompositeURL.class, name = "CompositeURL") }
+    @JsonSubTypes.Type(value = CompositeURL.class, name = "composite-url") }
 )
 public abstract class URL {
 
