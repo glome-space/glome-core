4d91df8cd187e946de4372a62cf070f4b8b682dd
michaellif <michael.lifschitz@gmail.com>
2017-11-19 07:34:38 -0500

    adding testPost assertion for response

--- glome-http/glome-http-executor/src/test/java/space/glome/http/executor/TestHttpExecutor.java
+++ glome-http/glome-http-executor/src/test/java/space/glome/http/executor/TestHttpExecutor.java
@@ -1,6 +1,7 @@
 package space.glome.http.executor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -13,25 +14,26 @@ import space.glome.http.schema.domain.HttpRequestItem;
 
 public class TestHttpExecutor {
 
-	
 	@Test
 	public void testGet() throws Exception {
 		HttpRequestItem httpRequestItem = unmarshal("/samples/request-item-001.json");
 		HttpRecord record = new HttpExecutor().exec(httpRequestItem.getRequest());
-		assertEquals(200, record.getResponse().getCode().intValue()); 
+		assertEquals(200, record.getResponse().getCode().intValue());
 	}
 
 	@Test
 	public void testPost() throws Exception {
 		HttpRequestItem httpRequestItem = unmarshal("/samples/request-item-002.json");
 		HttpRecord record = new HttpExecutor().exec(httpRequestItem.getRequest());
-		assertEquals(200, record.getResponse().getCode().intValue()); 
+		assertEquals(200, record.getResponse().getCode().intValue());
+		assertTrue("Response doesn't contain \"data\":\"Request Body\", responseBody=" + record.getResponse().getResponseBody(),
+				record.getResponse().getResponseBody().contains("\"data\": \"Request Body\""));
 	}
-	
+
 	private HttpRequestItem unmarshal(String requestFilePath) throws Exception {
 		return JsonMarshallingUtils.unmarshal(readFile(requestFilePath), HttpRequestItem.class);
 	}
-	
+
 	private String readFile(String filePath) throws IOException {
 		try (Scanner scanner = new Scanner(TestHttpExecutor.class.getResourceAsStream(filePath), "UTF-8")) {
 			return scanner.useDelimiter("\\A").next();
