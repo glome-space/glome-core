6f6bda4780281c3e3d26baf1c09e11849e529b58
michaellif <michael.lifschitz@gmail.com>
2017-12-14 08:32:41 -0500

    convert to interface

--- glome-http/glome-http-schema/src/main/java/space/glome/http/schema/domain/URL.java
+++ glome-http/glome-http-schema/src/main/java/space/glome/http/schema/domain/URL.java
@@ -1,5 +1,8 @@
 package space.glome.http.schema.domain;
 
+import java.net.URISyntaxException;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -8,6 +11,31 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
 @JsonSubTypes({ @JsonSubTypes.Type(value = RawURL.class, name = "raw-url"),
 		@JsonSubTypes.Type(value = CompositeURL.class, name = "composite-url") })
-public abstract class URL {
+public interface URL {
+
+	@JsonIgnore
+	String getScheme();
+
+	@JsonIgnore
+	String getUserInfo();
+
+	@JsonIgnore
+	String getHost();
+
+	@JsonIgnore
+	String getPath();
+
+	@JsonIgnore
+	Integer getPort() ;
+
+	@JsonIgnore
+	String getQuery();
+
+	@JsonIgnore
+	String getFragment();
+
+	@JsonIgnore
+	String getRaw() throws URISyntaxException;
+
 
 }
